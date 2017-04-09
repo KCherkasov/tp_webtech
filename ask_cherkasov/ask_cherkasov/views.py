01a6fdae36eabb76c6ad333e8da282a34a5dce55
KCherkasov <kvcherk@live.ru>
2017-04-09 19:17:41 +0300

    task 2

--- ask_cherkasov/ask_cherkasov/views.py
+++ ask_cherkasov/ask_cherkasov/views.py
@@ -5,44 +5,29 @@ from django.core.urlresolvers import reverse
 def base(request):
   return render(request, 'base.html', { 'test_auth' : True, 'best_users' : '1234567', 'hot_tags' : '12345', })
 
-def base_noauth(request):
-  return render(request, 'base.html', { 'test_auth' : False, })
-
 def index(request):
   return render(request, 'index.html', { 'test_auth' : True, 'best_users' : '1234567', 'hot_tags' : '12345', 'questions' : '0123456789', 'tags_count' : '123', })
-
-def index_noauth(request):
-  return render(request, 'index.html', { 'test_auth': False, 'best_users' : '1234567', 'hot_tags' : '12345', 'questions' : '0123456789', 'tags_count' : '1234', })
 
 def question(request):
   return render(request, 'question.html', { 'test_auth' : True, 'best_users' : '1234567', 'hot_tags' : '12345', 'answers' : '1234567', 'correct' : '5', 'tags_count' : '123', 'author_auth' : False, })
 
-def question_noauth(request):
-  return render(request, 'question.html', { 'test_auth' : False, 'best_users' : '1234567', 'hot_tags' : '12345', 'answers' : '1234567', 'correct' : '5', 'tags_count' : '123', 'author_auth' : False, })
-
-def question_author(request):
-  return render(request, 'question.html', { 'test_auth' : True, 'best_users' : '1234567', 'hot_tags' : '12345', 'answers' : '1234567', 'correct' : '5', 'tags_count' : '123', 'author_auth' : True, })
-
 def ask(request):
   return render(request, 'ask.html', { 'test_auth' : True, 'best_users' : '1234567', 'hot_tags' : '12345', 'errs' : False, })
-
-def ask_werrs(request):
-  return render(request, 'ask.html', { 'test_auth' : True, 'best_users' : '1234567', 'hot_tags' : '12345', 'errs' : '123', })
 
 def login(request):
   return render(request, 'login.html', { 'test_auth' : False, 'best_users' : '1234567', 'hot_tags' : '12345', 'errs' : False, })
 
-def login_werrs(request):
-  return render(request, 'login.html', { 'test_auth' : False, 'best_users' : '1234567', 'hot_tags' : '12345', 'errs' : '123', })
-
 def signup(request):
   return render(request, 'signup.html', { 'test_auth' : False, 'best_users' : '1234567', 'hot_tags' : '12345', 'errs' : False, })
-
-def signup_werrs(request):
-  return render(request, 'signup.html', { 'test_auth' : False, 'best_users' : '1234567', 'hot_tags' : '12345', 'errs' : '123', })
 
 def edit(request):
   return render(request, 'profile_edit.html', { 'test_auth' : True, 'best_users' : '1234567', 'hot_tags' : '12345', 'errs' : False, })
 
-def edit_werrs(request):
-  return render(request, 'profile_edit.html', { 'test_auth' : True, 'best_users' : '1234567', 'hot_tags' : '12345', 'errs' : '123', })
+def hello_world(request):
+  endl = '<br\>'
+  rstring = 'Hello world!' + endl
+  for key, val in request.GET.items():
+    rstring = rstring + str(key) + ' : ' + str(val) + endl
+  for key, val in request.POST.items():
+    rstring = rstring + str(key) + ' : ' + str(val) + endl
+  return HttpResponse(rstring)
