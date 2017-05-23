ae62dcc8f3b48c947291122a7990ec6472c8a8e0
KCherkasov <kvcherk@live.ru>
2017-05-23 13:13:48 +0300

    wsgi fixup

--- web_task_2/wsgi.py
+++ web_task_2/wsgi.py
@@ -1,27 +1,23 @@
+from cgi import parse_qs
+
 def application(environ, start_response):
   status = '200 OK'
-  endl = '\n';
-  rstring = 'Hello world!' + endl
-  rstring = rstring + 'REQUEST_METHOD : ' + environ['REQUEST_METHOD'] + endl
-  query = split_query(environ['QUERY_STRING'])
-  if query != '' : 
-    rstring = rstring + query + endl
-  if environ['REQUEST_METHOD'] == 'POST' :
-    try :
-      rstring = rstring + 'CONTENT_TYPE : ' + environ['CONTENT_TYPE'] + endl
-      rstring = rstring + 'CONTENT_LENGTH : ' + environ['CONTENT_LENGTH'] + endl
-      rstring = rstring + 'wsgi.input : ' + environ['wsgi.input'] + endl
-    except KeyError:
-      rstring = rstring + 'CONTENT_TYPE : ' + endl
-      rstring = rstring + 'CONTENT_LENGTH : ' + endl
-      rstring = rstring + 'wsgi.input : ' + str(environ['wsgi.input']) + endl
-  start_response(status, [ ("Content-Type", "text/plain"), ("Content-Length", str(len(rstring))), ])
-  return [rstring.encode('utf-8'), ]
-
-def split_query(qstring):
-  params = qstring.split('&')
-  if params == [] :
-    return ''
-  result = '\n'
-  result = result.join(params)
-  return result
+  endl = "\n";
+  urstring = '';
+  #rstring = bytes(urstring, 'UTF-8')
+  rstring = "<html>\n  <body>\n    <b>Hello world!</b>" + endl + "    <br /><hr />" + endl
+  rstring = rstring + "    <h3>REQUEST_METHOD : <b>" + environ['REQUEST_METHOD'] + "</b></h3>\n    <h3><b>GET</b> params:</h3>\n    <br />\n    <ul>"
+  query = parse_qs(environ['QUERY_STRING'])
+  for p in query:
+    rstring += "\n      <li>" + p + " : " + query[p][0] + " ;</li>"
+  rstring += endl + "    </ul>\n    <h3><b>POST</b> params:</h3>\n    <br />\n    <ul>\n"
+  request_body_size = int(environ.get('CONTENT_LENGTH', 0)) if environ.get('CONTENT_LENGTH') else 0
+  rstring += '      <li>CONTENT_LENGTH : ' + str(request_body_size) + " ;</li>"
+  request_body = environ['wsgi.input'].read(request_body_size)
+  post = parse_qs(request_body)
+  for key in post:
+    rstring += "\n      <li>" + str(key, 'UTF-8') + " : " + str(post[key][0], 'UTF-8') + " ;</li>"
+  #urstring = rstring.decode(encoding='UTF-8')
+  rstring += "\n    </ul>\n  </body>\n</html>\n"
+  start_response(status, [ ('Content-Type', 'text/html'), ('Content-length', str(len(rstring))) ])
+  return [bytes(rstring, 'UTF-8')]
